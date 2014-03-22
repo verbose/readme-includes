baf135c58a42c4279bf5b42183b65035043ec35a
jonschlinkert <dev@sellside.com>
2014-03-22 12:06:48 -0400

    adds `quickstart-yo` template.

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var path = require('path');
-var glob = require('globule');
-
-// Specify the directory to search
-var files = module.exports = (function () {
-  var dir = path.join(__dirname, 'templates/**').replace(/\\/g, '/');
-
-  // Return array of expanded filepaths.
-  return require('globule').find(dir, {
-    filter: 'isFile'
-  });
-})();
+var templates = require('path').join(__dirname, 'templates');
+module.exports = require('fs').readdirSync(templates).map(function(filepath) {
+  return require('path').resolve(templates, filepath);
+});
